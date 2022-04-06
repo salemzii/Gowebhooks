bcaf8475b5f1e12937fa88007570431a2b919caa
salemzii <salemododa2@gmail.com>
2022-04-06 23:15:54 +0100

    listening for paystack event

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/salemzii/Gowebhooks
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+}
+
+type PayHookResponse struct {
+	Event string     `json:"event"`
+	Data  DataStruct `json:"data"`
+}
+
+type DataStruct struct {
+	Customer_id    string         `json:"customer_id"`
+	Customer_code  string         `json:"customer_code"`
+	Email          string         `json:"email"`
+	Identification Identification `json:"Identification"`
+}
+
+type Identification struct {
+	Country        string `json:"country"`
+	Type           string `json:"type"`
+	Bvn            string `json:"bvn"`
+	Account_number string `json:"account_number"`
+	Bank_code      string `json:"bank_code"`
+}
+
+func ReceiveCustomerIsValidHook(w http.ResponseWriter, req *http.Request) {
+	var response PayHookResponse
+	if req.Method == "POST" {
+
+		respByte, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			log.Println(err)
+		}
+		e := json.Unmarshal(respByte, &response)
+		if e != nil {
+			log.Println(e)
+		}
+		log.Println(response)
+	}
+}
