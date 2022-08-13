37e490fd0ae4f7fad2645b46dd8dd94273ffc1a5
salemzii <salemododa2@gmail.com>
2022-08-13 15:55:17 +0100

    onesignal

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"text/template"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
+	/*
+		router := gin.Default()
 
-	router.GET("/", welcome)
-	router.POST("valid/response", ReceiveCustomerIsValidHook)
+		router.GET("/", welcome)
+		router.POST("valid/response", ReceiveCustomerIsValidHook)
 
-	router.Run()
+		router.Run()
+	*/
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+	http.ListenAndServe(":80", nil)
 }
 
 func welcome(c *gin.Context) {
@@ -48,4 +65,14 @@ func ReceiveCustomerIsValidHook(c *gin.Context) {
 		c.BindJSON(&response)
 		log.Println(response)
 	}
+}
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
 }
